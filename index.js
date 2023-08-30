96d2a8cf23430394a8c68c7cf714e60d5ac889ef
mohamed <matassi.mohamed89@gmail.com>
2023-08-30 19:18:03 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const users = {
+        name: userName,
+        email: userEmail
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(users)
+        //     name: 'Steve',
+        //     email: 'steve@steve.com'
+        // }),
+    })
+    .then(resp => resp.json())
+    .then(data => appendIdToDOM(data.id))
+    .catch(error => appendErrorToDOM(error.message))
+
+    function appendIdToDOM(id) {
+        const newId = document.createElement('span');
+        newId.textContent = `Newly Assigned ID: ${id}`;
+        document.body.appendChild(newId);
+    }
+
+    function appendErrorToDOM(message) {
+        const errorAlert = document.createElement('span');
+        errorAlert.textContent = `Error: ${message}`;
+        document.body.appendChild(errorAlert);
+    } 
+}
+
+submitData('Mohamed Matassi', 'mohamed@matassi.com');
+
